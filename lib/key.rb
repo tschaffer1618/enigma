b8bf8fb5011fa3908deb09cb86a97f68ba2b3e84
Tyler Schaffer <tpschaffer@gmail.com>
2019-07-24 21:43:37 -0600

    Create key class basic methods for encryption

--- lib/key.rb
+++ lib/key.rb
@@ -1,4 +1,14 @@
 class Key
+  attr_reader :number
 
+  def initialize(number)
+    @number = number
+  end
 
-end 
+  def split
+    { a_key: number.to_s[0..1].to_i,
+      b_key: number.to_s[1..2].to_i,
+      c_key: number.to_s[2..3].to_i,
+      d_key: number.to_s[3..4].to_i }
+  end
+end
--- test/key_test.rb
+++ test/key_test.rb
@@ -1,6 +1,20 @@
 require './test/test_helper'
 
 class KeyTest < Minitest::Test
+  def setup
+    @key = Key.new(35171)
+  end
 
+  def test_it_exists
+    assert_instance_of Key, @key
+  end
 
-end 
+  def test_attributes
+    assert_equal 35171, @key.number
+  end
+
+  def test_split_into_four
+    expected = {a_key: 35, b_key: 51, c_key: 17, d_key: 71}
+    assert_equal expected, @key.split
+  end
+end
