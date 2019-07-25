f8a40f7dd632ccc67e3288d52d3e3698c6cbc175
Tyler Schaffer <tpschaffer@gmail.com>
2019-07-24 22:04:59 -0600

    Create offset class basic methods for encryption

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,4 +1,22 @@
 class Offset
+  attr_reader :number
 
+  def initialize(number)
+    @number = number
+  end
 
-end 
+  def square
+    @number ** 2
+  end
+
+  def last_four_digits
+    square.to_s[-4..-1].to_i
+  end
+
+  def split
+    { a: last_four_digits.to_s[0].to_i,
+      b: last_four_digits.to_s[1].to_i,
+      c: last_four_digits.to_s[2].to_i,
+      d: last_four_digits.to_s[3].to_i }
+  end
+end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -1,6 +1,28 @@
 require './test/test_helper'
 
-class OffsetTest
+class OffsetTest < Minitest::Test
+  def setup
+    @offset = Offset.new(220817)
+  end
 
+  def test_it_exists
+    assert_instance_of Offset, @offset
+  end
 
-end 
+  def test_attributes
+    assert_equal 220817, @offset.number
+  end
+
+  def test_square
+    assert_equal 48_760_147_489, @offset.square
+  end
+
+  def test_last_four_digits
+    assert_equal 7489, @offset.last_four_digits
+  end
+
+  def split
+    expected = {a: 7, b: 4, c: 8, d: 9}
+    assert_equal expected, @offset.split
+  end 
+end
