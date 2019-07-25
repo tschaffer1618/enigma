e2939ceed048669c3cef74efcf6f097c4ce32201
Tyler Schaffer <tpschaffer@gmail.com>
2019-07-24 23:04:30 -0600

    Generate random key and date for today

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,19 @@
+require 'date'
+
 class Enigma
 
+  def random_key
+    ('%005d' % rand(10 ** 5)).to_i
+  end
 
-end 
+  def today
+    date = Date.today
+    day = ('%002d' % date.mday).to_s
+    month = ('%002d' % date.mon).to_s
+    year = date.year.to_s[2..3]
+    (day + month + year).to_i
+  end
+
+  def encrypt(message, key = random_key, date = today)
+  end
+end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,6 +1,21 @@
 require './test/test_helper'
 
 class EnigmaTest < Minitest::Test
+  def setup
+    @enigma = Enigma.new
+  end
 
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
 
-end 
+  def test_random_key
+    assert_instance_of Integer, @enigma.random_key
+    assert_equal 5, @enigma.random_key.digits.size
+  end
+
+  def test_today
+    assert_instance_of Integer, @enigma.today
+    assert_equal 6, @enigma.today.digits.size
+  end
+end
