57e26ff85985b7afb0143a4709ee9512612e1246
Tyler Schaffer <tpschaffer@gmail.com>
2019-07-25 20:58:38 -0600

    Create message class with apply_shift functionality

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,11 +9,6 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
-  def test_attributes
-    assert_equal ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "], @enigma.characters
-  end 
-
   def test_random_number_string
     assert_instance_of String, @enigma.random_number_string
     assert_equal 5, @enigma.random_number_string.length
