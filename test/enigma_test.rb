dca4ac1e31d96eeb8613efe0767e9b97c2387c6f
Tyler Schaffer <tpschaffer@gmail.com>
2019-07-27 21:07:43 -0600

    Add decrypt method to enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -29,4 +29,12 @@ class EnigmaTest < Minitest::Test
     @enigma.stubs(:random_number_string).returns("02715")
     assert_equal expected, @enigma.encrypt("hello world")
   end
+
+  def test_decrypt
+    expected = {:decryption=>"hello world", :key=>"02715", :date=>"040895"}
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
+
+    @enigma.stubs(:today).returns("040895")
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715")
+  end
 end
